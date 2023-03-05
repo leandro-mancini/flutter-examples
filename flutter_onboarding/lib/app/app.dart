940d7c4cc9278b9102d012a22fb7a448f1283dd9
Leandro Mancini <mancinilele@gmail.com>
2023-03-05 00:46:28 -0300

    feat(onboarding): create dot inticator

--- flutter_onboarding/lib/app/app.dart
+++ flutter_onboarding/lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_onboarding/app/widgets/dot_indicator_widget.dart';
 
 import 'widgets/page_widget.dart';
 
@@ -46,6 +47,19 @@ class _App extends State<App> {
                 currentPage = index;
               });
             },
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 80),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  for (int index = 0; index < pages.length; index++)
+                    DotIndicatorWidget(selected: index == currentPage)
+                ],
+              ),
+            ),
           )
         ],
       ),
