ce563619b1194545980ed68214a8cabb0642d7f2
Leandro Mancini <mancinilele@gmail.com>
2023-03-04 23:58:45 -0300

    feat(onboarding): create app home

---
+++ flutter_onboarding/lib/app/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _App();
+}
+
+class _App extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Onboarding'),
+      ),
+    );
+  }
+}
