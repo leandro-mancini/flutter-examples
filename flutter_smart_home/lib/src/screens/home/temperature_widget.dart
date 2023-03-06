58c212fe9ea97fed62203f5e233178d7ffbe8fde
Leandro Mancini <mancinilele@gmail.com>
2023-03-06 17:13:37 -0300

    feat(smart-home): update layout temperature

--- flutter_smart_home/lib/src/screens/home/temperature_widget.dart
+++ flutter_smart_home/lib/src/screens/home/temperature_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class TemperatureWidget extends StatelessWidget {
   const TemperatureWidget({Key? key}) : super(key: key);
@@ -6,93 +7,65 @@ class TemperatureWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(
-        top: 24,
-        left: 14,
-        right: 14,
-        bottom: 16
-      ),
       margin: const EdgeInsets.symmetric(horizontal: 24),
       decoration: const BoxDecoration(
-        color: Color(0xFF435C49),
-        borderRadius: BorderRadius.all(Radius.circular(16))
+        color: Color(0xFFFE844B),
+        borderRadius: BorderRadius.all(Radius.circular(12))
       ),
-      child: Column(
+      child: Stack(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Image.asset('assets/images/Sunny_Windy.png', height: 60,),
-                  const SizedBox(width: 10,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text('Sunny', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w700),),
-                      SizedBox(height: 6,),
-                      Text('Lagos, Nigeria', style: TextStyle(color: Colors.white, fontSize: 12, fontWeight: FontWeight.w400),)
-                    ],
-                  )
-                ],
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    '13',
-                    style: TextStyle(
-                      fontSize: 56,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.white
-                    ),
-                  ),
-                  Text(
-                    '°C',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.white
-                    ),
-                  )
-                ],
-              ),
-            ],
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Lottie.asset('assets/lottie/sun_animation.json', height: 100),
           ),
-          const SizedBox(height: 16,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text('16 km', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700, color: Colors.white),),
-                  Text('Visibility', style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400, color: Colors.white),),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text('48%', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700, color: Colors.white),),
-                  Text('Humidty', style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400, color: Colors.white),),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text('18 km/h', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700, color: Colors.white),),
-                  Text('W. Force', style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400, color: Colors.white),),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text('1009 hPa', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700, color: Colors.white),),
-                  Text('Pressure', style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400, color: Colors.white),),
-                ],
-              ),
-            ],
-          )
+          Padding(
+            padding: const EdgeInsets.only(left: 100, top: 12, bottom: 20, right: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Cloudy',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    SizedBox(height: 8,),
+                    Text(
+                      '18 Feb 2022',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w300
+                      ),
+                    ),
+                    SizedBox(height: 4,),
+                    Text(
+                      'Jagakarsa, Jakarta',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w300
+                      ),
+                    )
+                  ],
+                ),
+                const Text(
+                  '28 C',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
