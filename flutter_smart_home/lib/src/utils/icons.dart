1ce7ce750b320f89ee1985332f399944ae3f85dd
Leandro Mancini <mancinilele@gmail.com>
2023-03-06 23:44:14 -0300

    feat(smart-home): create utils icons

---
+++ flutter_smart_home/lib/src/utils/icons.dart
@@ -0,0 +1,6 @@
+class FIcons {
+  static String IconChart = 'assets/icons/icon_chart.svg';
+  static String IconHome = 'assets/icons/icon_home.svg';
+  static String IconCamera = 'assets/icons/icon_notification.svg';
+  static String IconSetting = 'assets/icons/icon_perfil.svg';
+}
