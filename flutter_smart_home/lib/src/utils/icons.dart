3bc05c40d957452c24e3a7623261c3d41485d366
Leandro Mancini <mancinilele@gmail.com>
2023-03-07 15:57:44 -0300

    feat(smart-home): add icon in class FIcons

--- flutter_smart_home/lib/src/utils/icons.dart
+++ flutter_smart_home/lib/src/utils/icons.dart
@@ -3,4 +3,9 @@ class FIcons {
   static String IconHome = 'assets/icons/icon_home.svg';
   static String IconCamera = 'assets/icons/icon_notification.svg';
   static String IconSetting = 'assets/icons/icon_perfil.svg';
+
+  static String IconAirConditioner = 'assets/icons/icon_air_conditioner.svg';
+  static String IconGoogleSmartHome = 'assets/icons/icon_google_smart_home.svg';
+  static String IconSmartLamp = 'assets/icons/icon_smart_lamp.svg';
+  static String IconSmartTv = 'assets/icons/icon_smart_tv.svg';
 }
